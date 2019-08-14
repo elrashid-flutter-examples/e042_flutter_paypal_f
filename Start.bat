a221677bfe401b6f1737101357e4adf464b9e4fc
moh <osman.elrashid@gmail.com>
2019-08-14 12:23:01 +0400

    initial files

---
+++ Start.bat
@@ -0,0 +1,5 @@
+call env.bat 
+cd e042_flutter_paypal_f 
+start ConEmu.exe -runlist cmd /K run.emulator.bat ^|^|^| cmd /K ping 127.0.0.1 -n 6 ^> nul ^& run.app.bat 
+call Code .>nul
+Exit 
---
+++ git.init.bat
@@ -0,0 +1,5 @@
+Set HUB_PROTOCOL=ssh 
+git init 
+git add . && git commit -m "initial files" 
+hub create elrashid-flutter-examples/e042_flutter_paypal_f 
+git push --set-upstream origin master 
